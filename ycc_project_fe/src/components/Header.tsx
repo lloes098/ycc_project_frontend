5806eaa5f08cdacac11b3eed9be8d1658ac52954
jianbaek <jian0314@yonsei.ac.kr>
2025-06-02 01:06:32 +0900

    헤더 수정

---
+++ ycc_project_fe/src/components/Header.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import BackButton from './BackButton';
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+  background-color: #fff;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 8px 16px;
+  background-color: #fff;
+  position: relative;
+  max-width: 768px;
+  margin: 0 auto;
+`;
+
+const HeaderTitle = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+interface HeaderProps {
+  title: string;
+  showBackButton?: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({ title, showBackButton = true }) => {
+  return (
+    <HeaderWrapper>
+      <HeaderContainer>
+        {showBackButton && <BackButton />}
+        <HeaderTitle>{title}</HeaderTitle>
+      </HeaderContainer>
+    </HeaderWrapper>
+  );
+};
+
+export default Header; 
